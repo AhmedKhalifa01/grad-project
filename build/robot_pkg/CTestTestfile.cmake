072456cfef0dc768c1f37732fc197c56e28dc938
AhmedKhalifa01 <91398@must.edu.eg>
2023-12-17 03:24:15 +0200

    Our Grad Project

---
+++ build/robot_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmedkhalifa/graduation_ws/src/robot_pkg
+# Build directory: /home/ahmedkhalifa/graduation_ws/build/robot_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
